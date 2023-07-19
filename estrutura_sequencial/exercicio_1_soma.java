65d8a2fd73497809737c441cd99dd910fd76d87d
marcoosdantas <2226973@aluno.univesp.br>
2023-07-19 18:24:19 -0300

    Create exercicio_1_soma.java

---
+++ estrutura_sequencial/exercicio_1_soma.java
@@ -0,0 +1,15 @@
+package exercicio_1;
+
+import java.util.Scanner;
+
+public class exercicio_1_soma {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int s1, s2;
+		s1 = sc.nextInt();
+		s2 = sc.nextInt();
+		System.out.println("SOMA= " + (s1 + s2));
+	}
+
+}
